6034a5553530b37c38500bf63a0ca60d158939d3
Yakare2025 <yakare.cosmetics.info@gmail.com>
2025-02-27 16:39:57 +0100

    Ajout du projet

--- main.go
+++ main.go
@@ -1,13 +1,1 @@
-package main
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, Go!"})
-	})
-	r.Run(":8080") // Lancer le serveur
-}
+ 
